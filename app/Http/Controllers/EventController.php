77e7530416332199712a68d6c919814514c0fb94
J-J-T-M <jonatham.junior.18@gmail.com>
2022-12-21 17:05:16 -0400

    Image Upload

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -35,10 +35,24 @@ class EventController extends Controller
         $event->city = $request->city ;
         $event->private = $request->private ;
         $event->description = $request->description ;
+
+        // Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid())
+        {
+            $requestImage = $request->image;
+            
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;// Criar um nome unico adicionando no nome original da image com o tempo 
+        
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        } 
          
         $event->save();
 
-        return redirect(route('events.index'));
+        return redirect(route('events.index'))->with('msg', "Evento criado com sucesso!");
 
     }
 }

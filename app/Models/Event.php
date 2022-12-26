94c9a2a87cfe4aafe8abbff4678a53ac98701212
J-J-T-M <jonatham.junior.18@gmail.com>
2022-12-26 19:11:12 -0400

    casts dos item e relação a o bd user

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
@@ -15,4 +16,15 @@ class Event extends Model
     //     'description',
     //     'private'
     // ];
+    protected $casts = [
+        'items' => 'array'
+    ];
+    protected $dates = [
+        'date'
+    ];
+
+    public function user()
+    {
+        return $this->BelongsTo('App\Models\User');
+    }
 }

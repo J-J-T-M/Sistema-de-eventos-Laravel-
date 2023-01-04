fc06f4ed317c477f96628b5e57f0df2f30012e73
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-04 17:11:24 -0400

    leaveevent

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,4 @@ Route::put('/events/update/{id}', [EventController::class, 'update'])->middlewar
 
 Route::post('/events/join/{id}', [EventController::class, 'joinEvent'])->middleware('auth');
 
+Route::delete('/events/leave/{id}', [EventController::class, 'leaveEvent'])->middleware('auth');

4ca5601f68974ebe050dbe43672427905fe78af9
J-J-T-M <jonatham.junior.18@gmail.com>
2023-01-04 17:12:41 -0400

    event que estou participando

--- resources/views/events/dashboard.blade.php
+++ resources/views/events/dashboard.blade.php
@@ -41,5 +41,41 @@
            <p>Você ainda não tem eventos, <a href="/events/create">criar evento</a></p> 
         @endif
     </div>
+    <div class="col-md-10 offset-md-1 dashboard-title-container">
+        <h1>Eventos inscritos</h1>
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
+        @if (count($eventsAsParticipant) > 0)
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Participantes</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($eventsAsParticipant as $event)
+                        <tr>
+                            <td scropt='row'>{{ $loop->index = 1 }}</td>
+                            <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                            <td>{{ count($event->users) }}</td>
+                            <td>
+                                <form action="/events/leave/{{ $event->id }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                     
+                                        <button type="submit" class="btn btn-danger exit-btn"><ion-icon name="exit-outline"></ion-icon>Cancelar inscrição</button>
+                                    </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table> 
+        @else
+            <p>Você ainda não está participante de nenhum evento, <a href="/">veja todos os eventos</a></p>
+        @endif
+    </div>
 
 @endsection
